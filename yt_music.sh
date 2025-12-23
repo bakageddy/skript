2f59bc032f6ac9d6fc9d5506111cadb3bf86bfb0
bakageddy <gdineshda@gmail.com>
2025-12-23 15:38:38 +0530

    yt update settings

--- yt_music.sh
+++ yt_music.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
 set -xe
-yt-dlp --embed-chapters -o "%(playlist_index)03d-%(title)s.%(ext)s" -f "bestaudio[ext=m4a]/best[ext=mp4]/best" $@
+yt-dlp -N $(nproc) --embed-chapters -o "%(playlist_index)03d-%(title)s.%(ext)s" -f "bestaudio[ext=m4a]/best[ext=mp4]/best" $@
--- yt_vid.sh
+++ yt_vid.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
 set -xe
-yt-dlp --write-auto-subs --sub-langs "en" --embed-chapters --embed-subs -o "%(playlist_index)03d-%(title)s.%(ext)s" $@
+yt-dlp --write-auto-subs --sub-langs "en" --embed-chapters --embed-subs -o "%(playlist_index)03d-%(title)s.%(ext)s" -N $(nproc) $@
